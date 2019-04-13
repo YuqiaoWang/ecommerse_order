58ded8d348d1762877e84c791e91aa6b37222068
YuqiaoWang <yuqiaowang1993@outlook.com>
2019-04-13 22:59:21 +0800

    initial project

---
+++ src/main/java/com/cmb/ccc/ecommerce_order_system/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.cmb.ccc.ecommerce_order_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
