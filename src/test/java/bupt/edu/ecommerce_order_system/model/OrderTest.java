1ce46bdb7881fe175a32e4fc671dd8afef163850
YuqiaoWang <yuqiaowang@bupt.edu.cn>
2019-09-24 21:26:07 +0800

    copyright

---
+++ src/test/java/bupt/edu/ecommerce_order_system/model/OrderTest.java
@@ -0,0 +1,20 @@
+package bupt.edu.ecommerce_order_system.model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class OrderTest {
+    Order order = new Order();
+
+    @Test
+    public void test() {
+
+    }
+}
